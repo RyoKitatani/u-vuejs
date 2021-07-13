7e162e53253a963bea1905524d6d4f4d8dea54b2
RyoKitatani <ryo0929m@gmail.com>
2021-07-14 01:12:51 +0900

    finish 6-84

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-  <LikeHeader></LikeHeader>
+  <like-header></like-header>
+  <!-- ケバブケース -->
   <LikeNumber></LikeNumber>
   </div>
 </template>
@@ -10,8 +11,14 @@ import LikeHeader from "./components/LikeHeader.vue"
 
 export default {
  components: {
-   LikeHeader
+   LikeHeader: LikeHeader
+   //パスカルケース 大文字で区切る
  } 
 }
 </script>
 
+// ルールとしては、コンポーネントはパスカルケース 統一する
+// ケバブケースが用意されている理由、ブラウザが大文字小文字の区別ができない
+// JSの使用から文字識別ができる仕組みになっているから識別可能
+
+// キャメルケース 最初の文字が小文字
