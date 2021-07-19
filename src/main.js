4f11fab97980a81c45d10d99e31924783ce0fcca
RyoKitatani <ryo0929m@gmail.com>
2021-07-19 10:36:39 +0900

    finish 9-136

--- src/main.js
+++ src/main.js
@@ -4,26 +4,29 @@ import LikeNumber from './components/LikeNumber.vue'
 
 Vue.config.productionTip = false
 Vue.component('LikeNumber', LikeNumber)
-Vue.directive("border", {
+Vue.directive("border", function(el, binding) {
+  el.style.border = "solid black 2px"
+} );
   // 5つのライフスタイルフック関数
 
-  bind(el, binding, vnode) {
+  // bind(el, binding, vnode) {
     // ディレクティブが初めて対象の要素に紐づいた時
-  },
-  inserted(el, binding, vnode) {
-  //  親Nodeに挿入された時
-  },
-  update(el, binding, vnode, oldVnode) {
-    // コンポーネントが更新される度。子コンポーネントが更新される前
-  },
-  componentUpdated(el, binding, vnode, oldVnode) {
-    // コンポーネントが更新される度。子コンポーネントが更新された後
-  },
-  unbind(el, binding, vnode) {
-    // ディレクティブが紐づいている要素から取り除かれた時
-  },
+  // },
 
-} )
+  // inserted(el, binding, vnode) {
+  // //  親Nodeに挿入された時
+  // },
+
+  // update(el, binding, vnode, oldVnode) {
+    // コンポーネントが更新される度。子コンポーネントが更新される前
+  // }
+
+  // componentUpdated(el, binding, vnode, oldVnode) {
+  //   // コンポーネントが更新される度。子コンポーネントが更新された後
+  // },
+  // unbind(el, binding, vnode) {
+  //   // ディレクティブが紐づいている要素から取り除かれた時
+  // },
 
 
 new Vue({
