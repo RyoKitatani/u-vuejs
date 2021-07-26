3ded622d46f160da1888d71abcbfd4ba7ee36f71
RyoKitatani <ryo0929m@gmail.com>
2021-07-26 11:31:01 +0900

    finish 14-217

--- src/views/HeaderHome.vue
+++ src/views/HeaderHome.vue
@@ -9,11 +9,17 @@
 </template>
 
 <script>
-import { mapMutations } from "vuex";
+// import { mapMutations } from "vuex";
 
 export default {
   methods: {
-    ...mapMutations(["increment", "decrement"])
+    // ...mapMutations(["increment", "decrement"])
+    increment() {
+      this.$store.dispatch('increment', 2)
+    },
+    decrement() {
+      this.$store.dispatch('decrement', 2)
+    }
   }
 }
 </script>
