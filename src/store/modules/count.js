b578fe2157a3571775d11e47e196a65032540f2d
RyoKitatani <ryo0929m@gmail.com>
2021-07-26 12:55:28 +0900

    finish 14-221

---
+++ src/store/modules/count.js
@@ -0,0 +1,23 @@
+const state = {
+  count : 2
+};
+
+const getters = {
+  doubleCount: state => state.count * 2,
+  tripleCount: state => state.count * 3,
+}
+
+const mutations = {
+  increment(state, number) {
+    state.count += number;
+  },
+  decrement(state, number) {
+    state.count -= number;
+  },
+}
+
+export default {
+  state,
+  getters,
+  mutations
+}
